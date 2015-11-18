164e3c4253c9c59aab44478a604f29126902ea96
rein <lain@lain.org.uk>
2015-11-18 23:29:09 +0000

    offset retrieval done

--- Main.fs
+++ Main.fs
@@ -7,15 +7,18 @@ module Telegram =
   let JSONEXN = "An issue was encountered parsing the JSON object."
   let HTTPEXN = "An issue was encountered contacting the server."
 
-  //  (?=) :: JsonValue -> a -> Some JsonValue | None
+  //  (?=) :: JsonValue -> a -> option JsonValue 
   let (?=) (value: JsonValue) prop =
     value.TryGetProperty prop
+
+  //  succ :: int -> int
+  let succ x = x + 1
 
   //  getEndpoint :: string -> string
   let getEndpoint token path = 
     "https://api.telegram.org/bot" + token + "/" + path
 
-  //  getUpdates :: string -> Some JsonValue | None
+  //  getUpdates :: string -> option JsonValue 
   let getUpdates token =
     try 
       getEndpoint token "getUpdates" |> 
@@ -24,21 +27,38 @@ module Telegram =
       | :? System.Net.WebException -> printfn "%s" HTTPEXN ; None
       | _                          -> printfn "%s" JSONEXN ; None
 
+  //  getUpdatesO :: string -> int -> option JsonValue
+  let getUpdatesO token offset =
+    try
+      let soffset = sprintf "%d" offset
+      getEndpoint token "getUpdates" |>
+        (fun url -> Http.RequestString(url, query=["offset", soffset]))
+    with
+      | :? System.Net.WebException -> printfn "%s" HTTPEXN ; "" 
+      | _                          -> printfn "something happened." ; "" 
+
   //  verifyPoll :: JsonValue -> bool
   let verifyPoll (poll: JsonValue) = 
     match (poll ?= "ok") with
       | Some jval -> jval.AsBoolean ()
       | None      -> false
 
-  //  result :: JsonValue -> [JsonValue]
+  //  result :: JsonValue -> option JsonValue
   let result (poll: JsonValue) =
     poll ?= "result"
+
+  //  message_id :: JsonValue -> option int
+  let message_id msg =
+    msg ?= "message_id" |> Option.map (fun (x: JsonValue) -> x.AsInteger ())
+    
+
+  //  newUpdateId :: JsonValue -> int
+  //  let newUpdateId (msgs: JsonValue) =
 
 module Main =
   [<EntryPoint>]
   let main args =
-    let update = Telegram.getUpdates ""
-    match Option.map Telegram.result update with
-      | Some res -> printfn "%O" res
-      | None     -> printfn "No results, or malformed update."
+    let token = ""
+    let update = Telegram.getUpdatesO token 10
+    printfn "%d" <| newUpdateId update
     0
